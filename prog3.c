317288bccd9673c1131c7839ab5e1e620fc7f348
bhargavgorpade <bhargavgorpade@gmail.com>
2018-09-14 09:26:26 +0530

    Create prog3.c

---
+++ prog3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+  char c;
+  printf("\n Enter a character : ");
+  if(c<a&&c>Z)
+    {
+      printf("\nInvalid");
+    }
+    else
+    {
+      if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+      {
+        printf("\nVowel");
+      }
+      else
+      {
+        printf("\nConsonant");
+      }
+    }
+  }  
